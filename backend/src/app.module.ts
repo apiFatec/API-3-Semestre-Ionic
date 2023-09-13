469748538ce042b233900e5630cfbcdb347fa2e3
ImBard <nicota1234@gmail.com>
2023-09-13 12:20:03 -0300

    config db connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { UserModule } from './app/user/user.module';
+require('dotenv').config()
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TypeOrmModule.forRoot({
+    type: process.env.TYPEORM_CONNECTION,
+    port: Number(process.env.TYPEORM_PORT),
+    host: process.env.TYPEORM_HOST,
+    database: process.env.TYPEORM_DATABASE,
+    username: process.env.TYPEORM_USERNAME,
+    password: process.env.TYPEORM_PASSWORD,
+    entities: [__dirname + '/*/*/.entity{.ts,.js}'],
+    synchronize: false
+  } as TypeOrmModuleOptions),
+    UserModule,
+  ],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }

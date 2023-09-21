0743d9fce8c7ed21b47a26f98e20d6d43ea6cc49
Caio Yokoyama <111662298+Caboia@users.noreply.github.com>
2023-09-21 12:00:26 -0300

    :sparkles: feat: Tela Home

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -1,38 +1,69 @@
 import IonicText from "../../assets/IonicText.png";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useEffect, useState } from "react";
 
 export function Home() {
+  const [findAll, setDadosDoProcesso] = useState(null);
+  
+  useEffect(() => {
+    async function carregarDadosDoProcesso() {
+      const dados = await findAll();
+      if (dados) {
+        setDadosDoProcesso(dados); 
+      }
+    }
+
+    carregarDadosDoProcesso();
+  }, []);
+
   return (
-    <body style={{ marginLeft: "4rem" }}>
-      <nav>Aqui vai ter a nav</nav>
-      <img src={IonicText} alt="Minha Imagem" />
-      <p style={{ color: "#6A6A6A" }}>Acompanhamento de processos</p>
-      <main
-        style={{
-          display: "grid",
-          gridTemplateColumns: "1fr 1fr 1fr",
-          gap: "20px",
-          marginTop: "4rem",
-        }}
-      >
-        <section>
-          <h3>Backlog</h3>
-          <div
-            style={{
-              marginTop: "2rem",
-              maxWidth: "15.938rem",
-              display: "flex",
-              padding: "4rem",
-              borderRadius: "6px",
-              boxShadow: "0px 0px 6px 0px rgba(0, 0, 0, 0.25)",
-            }}
-          ></div>
-        </section>
-        <section>
-          <h3>Em Andamento</h3>
-        </section>
-        <section>
-          <h3>Finalizado</h3>
-        </section>
+    <body className="mx-16">
+      <nav className="p-8">nav</nav>
+      <section className="flex justify-between">
+        <div>
+          <img src={IonicText} alt="Minha Imagem" />
+          <p className="text-gray-600">Acompanhamento de processos</p>
+        </div>
+        <div className="flex gap-[1rem] items-end mr-[8rem]">
+          <Button className="border-solid border-[1px] border-gray-200 bg-white text-gray-400 font-normal text-[9pt] active:bg-ionic-pressed hover:bg-ionic-normal hover:text-white w-[8rem] h-12 hover:border-hidden">
+            Novo processo
+          </Button>
+          <Input
+            className="h-12 w-[25rem] placeholder:text-gray-400 "
+            placeholder="Pesquisar processo..."
+          ></Input>
+        </div>
+      </section>
+      <main className="grid grid-cols-3 gap-20px mt-16">
+      if (dadosDoProcesso) {
+        return (
+          <section>
+            <h3>Backlog</h3>
+            <div className="mt-8 flex-column p-[1.4rem] h-[10rem] max-w-[400px] rounded-md shadow-[0px_0px_6px_0px_rgba(0,0,0,0.25)]">
+              <p className="text-gray-600 font-light text-[14pt]">
+              {findAll.name}
+              </p>
+            </div>
+          </section>
+          <section>
+            <h3>Em Andamento</h3>
+            <div className="mt-8 flex-column p-[1.4rem] h-[10rem] max-w-[400px] rounded-md shadow-[0px_0px_6px_0px_rgba(0,0,0,0.25)]">
+              <p className="text-gray-600 font-light text-[14pt]">
+              {findAll.name}
+              </p>
+            </div>
+          </section>
+          <section>
+            <h3>Finalizado</h3>
+            <div className="mt-8 flex-column p-[1.4rem] h-[10rem] max-w-[400px] rounded-md shadow-[0px_0px_6px_0px_rgba(0,0,0,0.25)]">
+              <p className="text-gray-600 font-light text-[14pt]">
+              {findAll.name}
+              </p>
+            </div>
+          </section>
+  )
+}
       </main>
     </body>
   );

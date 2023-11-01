7f3b1b635da9488bf148afc43b148cea8a8241f2
giovaniavila <giovanicarvalhoavila@hotmail.com>
2023-11-01 19:28:30 -0300

    feat: tela página não encontrada

--- frontend/src/routes/routes.tsx
+++ frontend/src/routes/routes.tsx
@@ -9,6 +9,7 @@ import { CadastroProcessos } from "@/pages/cadastroProcesso/cadastroProcesso";
 import { Process } from "@/pages/process/process";
 import { EditarEquipe } from "@/pages/editarEquipe/editarEquipe";
 import { TelaTarefas } from "@/pages/telaTarefas/telaTarefas";
+import { PaginaNaoEncontrada } from "@/pages/paginaNaoEncontrada/paginaNaoEncontrada";
 
 export function Router() {
   return (
@@ -20,14 +21,16 @@ export function Router() {
       <Route element={<PrivateRoutes />}>
         <Route path="/admin-register" element={<Register />} />
       </Route>
-      <Route element={<LoginRoutes />}>
         <Route path="/criar-equipe/" element={<EditarEquipe />} />
         <Route path="/criar-processo" element={<CadastroProcessos />} />
         <Route path="/processos" element={<Home />} />
         <Route path="/telaTarefas/:process/:id" element={<TelaTarefas />} />
         <Route path="/" element={<Home />} />
         <Route path="/processos/:process/:id" element={<Process />} />
+      <Route element={<LoginRoutes />}>
       </Route>
+
+      <Route path="*" element={<PaginaNaoEncontrada />} />
     </Routes>
   );
 }

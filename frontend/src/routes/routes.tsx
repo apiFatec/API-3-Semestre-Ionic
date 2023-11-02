c37e12e21ed5740b2edc971610f5ca7536ec61f1
MateusdiSousa <mateus69107@gmail.com>
2023-11-02 11:08:47 -0300

    :fire: Visualizar equipe

--- frontend/src/routes/routes.tsx
+++ frontend/src/routes/routes.tsx
@@ -8,7 +8,6 @@ import IsLogged from "./IsLogged";
 import { CadastroProcessos } from "@/pages/cadastroProcesso/cadastroProcesso";
 import { Process } from "@/pages/process/process";
 import { EditarEquipe } from "@/pages/editarEquipe/editarEquipe";
-import { VisualizarEquipe } from "@/pages/visualizarEquipe/VisualizarEquipe";
 import { CadastroIso } from "@/pages/cadastroIso/cadastroIso";
 
 export function Router() {
@@ -27,7 +26,6 @@ export function Router() {
         <Route path="/processos" element={<Home />} />
         <Route path="/" element={<Home />} />
         <Route path="/processos/:process/:id" element={<Process />} />
-        <Route path="/equipe" element={<VisualizarEquipe />} />
         <Route path="/isos" element={<CadastroIso />} />
       </Route>
     </Routes>
